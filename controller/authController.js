c7fb959407549a94dc0a193c2fcdee5bb3d18cd3
aditya <adityakhattri123@gmail.com>
2024-07-05 12:50:26 +0530

    added errorMiddleware

--- controller/authController.js
+++ controller/authController.js
@@ -1,44 +1,55 @@
 const UserModel = require("../database/models/userModel");
 const jwt = require('jsonwebtoken');
+const ErrorHandler = require("../utils/errorHandler");
+const asyncErrorHandler = require("../middleware/asyncErrors");
 
 
-const signupUser = async (req,res,next)=>{
+const signupUser = asyncErrorHandler(async (req,res,next)=>{
     const {username , password} = req.body;
+
     const existingUser = await UserModel.findOne({username});
     
-    if(existingUser) return  res.status(400).json({status:false , message:'Username in use please select something else'});
+    if(existingUser) return next(new ErrorHandler("Username exists",409))
 
     const newUser = await UserModel.create({username,password})
 
     const authToken = jwt.sign({id:newUser._id.toString()},process.env.JWT_SECRET);
     
-        res.status(200).cookie("user", authToken, { httpOnly: true, secure: true, sameSite: "Strict", maxAge: 3600000, path: "/" })
+        res.status(200)
+        .cookie("user", authToken, { httpOnly: true, secure: true, sameSite: "Strict", maxAge: 3600000, path: "/" })
         .json({ message: "Created", status: true })
 
-}
+});
 
-const login = async (req, res) => {
+const login = asyncErrorHandler(async (req, res) => {
         const { username, password } = req.body
+
         const existingUser = await UserModel.findOne({ username })
-        if (!existingUser) return res.status(401).json({ message: "User doesn't exists", status: false })
+        
+        if (!existingUser) return next(new ErrorHandler("Invalid Credentials",401));
+        
         const hash = existingUser.password
+        
         const match = await bcrypt.compare(password, hash)
-        if (!match) return res.status(401).json({ message: "Wrong Credentials", status: false })
+        
+        if (!match) return next(new ErrorHandler("Invalid Credentials",401))
+        
         const authToken = jwt.sign(existingUser._id.toString(), process.env.JWT_SECRET)
+        
         res.cookie("user", authToken, { httpOnly: true, secure: true, sameSite: "Strict", maxAge: 3600000, path: "/" })
         res.status(200).json({ message: "Logged In", status: true })
-}
+})
 
-const logout = async (req,res,next)=>{
+const logout = asyncErrorHandler(async (req,res,next)=>{
     res.status(200).cookie("user", "", { httpOnly: true, secure: true, sameSite: "Strict", path: "/" })
     .json({ message: "Logged Out", status: true })
-}
+})
 
-const getDetails = async (req, res) => {
+const getDetails = asyncErrorHandler(async (req, res) => {
 
         const user = await UserModel.findOne({ _id: req.id })
         res.status(200).json({ user, status: true })
-}
+})
 
 module.exports ={
     signupUser,
--- server.js
+++ server.js
@@ -2,18 +2,23 @@ const express = require("express");
 require('dotenv').config();
 require("./database/connect");
 const authRouter = require("./routes/authRouter")
+const cookieParser = require("cookie-parser")
+const errorMiddleware = require("./middleware/errorMiddleware");
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/auth",authRouter)
 
 app.get("/ping",(req,res)=>{
     res.status(200).json({message:"pong"})
 })
+
+app.use(errorMiddleware)
 
 app.listen(port,()=>{
     console.log(`Server is listening at port ${port}`);
