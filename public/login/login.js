4ea7e72a1fc5def7813b3692cbe90088da41b805
aditya <adityakhattri123@gmail.com>
2024-07-06 14:32:43 +0530

    added initial static files

---
+++ public/login/login.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('login-form');
+
+form.addEventListener("submit",async (e)=>{
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const sendPayload = {
+        username: formData.get("username"),
+        password: formData.get("password")
+    }
+    const response = await fetch("/auth/login",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+        },
+        body:JSON.stringify(sendPayload)
+    })
+    const result = await response.json();
+    if(result.status === 200){
+        alert("Login")
+    }else{
+        window.location.reload();
+        alert(result.message)
+    }
+})
